5b008b36e3bb60ac8dc7c107848f7dcc1a29eada
MichalZiolkiewicz <m.ziolkiewicz1@gmail.com>
2021-06-24 18:21:54 +0200

    Working display of movie details

--- src/components/SingleFilmPage.vue
+++ src/components/SingleFilmPage.vue
@@ -1,21 +1,61 @@
 <template>
-  <div>
-    <p>{{$route.params.id}}</p>
+  <div class="wrapper" v-if="resultData">
+    <div>
+      <h1>{{resultData.title}}</h1>
+      <img src="" alt="">
+    </div>
+    <div class="infoBox">
+<!--    gatunek, link IMDB, opis, kraj produkcji -->
+      <ul>
+        <li v-for="genre in resultData.genres" :key="genre.id">{{genre.name}}</li>
+        <li v-for="country in resultData.production_countries" :key="country.iso_3166_1">Kraj produkcji: {{country.name}}</li>
+        <li>{{resultData.imdb_id}}</li>
+      </ul>
+      <div>
+        {{resultData.overview}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "SingleFilmPage",
-  props: {
-    id: Number
+  data() {
+    return {
+      resultData: null,
+      language: 'pl-PL',
+      API_KEY: '6867970f578bc54a2df62f33811ee300'
+    }
   },
   mounted() {
-    console.log('Zamontowane gotowe do fetchowania');
+    axios
+      .get(this.API_MOVIE)
+      .then(result => this.resultData = result.data)
+      .catch(err => console.log(err))
+  },
+  computed: {
+    API_MOVIE() {
+      return `https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=${this.API_KEY}&language=${this.language}`
+    }
   }
 }
 </script>
 
 <style scoped>
+
+  .wrapper{
+    max-width: 1440px;
+    background-color: #dfe6e9;
+    height: 100%;
+    min-height: 100vh;
+    margin: 0 auto;
+  }
+
+  .infoBox{
+    background-color: white;
+  }
 
 </style>
